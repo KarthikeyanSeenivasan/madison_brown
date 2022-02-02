8f4e051f2dab1b96dc3d52c38eca5303e1093b99
karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-02-02 17:37:00 +0530

    congig changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint:
+          "https://madisonbrownicecream.us14.list-manage.com/subscribe/post?u=cc8239a65f07d2b1871ac509f&amp;id=a2d5a9b530",
+        timeout: 3500,
+      },
+    },
+  ],
 }
